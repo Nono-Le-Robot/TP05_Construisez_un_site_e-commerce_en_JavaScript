ab03dfef9a571c5d0eab9072542d767833bf9938
nono <renaud.sannier@laposte.net>
2022-05-21 13:03:46 +0200

    ajout panier et recupération des données local storage

---
+++ front/js/cart.js
@@ -0,0 +1,105 @@
+const cartItemSelector = document.querySelector("#cart__items")
+const cartPriceSelector = document.querySelector("#cart__price")
+
+
+function saveBasket(basket){
+    localStorage.setItem("basket", JSON.stringify(basket))
+}
+
+function getBasket(){
+    let basket = localStorage.getItem("basket")
+    if(basket == null){
+        return [];
+    }
+    else{
+        return JSON.parse(basket)
+    }
+}
+
+function addBasket(product){
+    let basket = getBasket();
+    let foundProduct = basket.find(p => p.id == product.id);
+    if(foundProduct != undefined){
+        foundProduct.quantity++;
+    }
+    else{
+        product.quantity = 1;
+        basket.push(product)
+    }
+    saveBasket(basket)
+}
+
+function changeQuantity(product,quantity){
+    let basket = getBasket();
+    let foundProduct = basket.find(p => p.id == product.id)
+    if(foundProduct != undefined){
+        foundProduct.quantity += quantity.value;
+    }
+    saveBasket(basket)
+}
+
+var indexArray = 0;
+var colorsPush = [];
+var quantityPush = [];
+var quantityPushTotal = 0;
+var pricePush = 0;
+var total = 0;
+let order = getBasket()
+
+for(var i = 0; i < order.length; i++){
+    const url = `http://localhost:3000/api/products/${order[i].id}`
+    colorsPush.push(order[i].color)
+    
+    quantityPush.push(order[i].quantity)
+    quantityPushTotal += order[i].quantity
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        cartItemSelector.innerHTML +=
+            `
+            <article class="cart__item" data-id="${data.id}" data-color="${data.colors}">
+            <div class="cart__item__img">
+            <img src="${data.imageUrl}" alt="${data.altTxt}">
+            </div>
+            <div class="cart__item__content">
+            <div class="cart__item__content__description">
+            <h2>${data.name}</h2>
+            <p>${order[indexArray].color}</p>
+            <p>${data.price * order[indexArray].quantity} €</p>
+            </div>
+            <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+            <p>Qté : </p>
+            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${order[indexArray].quantity}">
+            </div>
+            <div class="cart__item__content__settings__delete">
+            <p class="deleteItem">Supprimer</p>
+            </div>
+            </div>
+            </div>
+            </article>
+            `
+            total += data.price * order[indexArray].quantity
+            indexArray++
+            totalPrice.innerHTML =
+            `
+            ${total}
+            `
+            totalQuantity.innerHTML =  
+            `
+            ${quantityPushTotal}
+            `
+            const itemQuantitySelector = document.querySelector('.itemQuantity')
+            
+
+        })
+    }
+
+
+
+
+
+
+
+
+
--- front/js/product.js
+++ front/js/product.js
@@ -1,32 +1,75 @@
+function saveBasket(basket){
+    localStorage.setItem("basket", JSON.stringify(basket))
+}
+function getBasket(){
+    let basket = localStorage.getItem("basket")
+    if(basket == null){
+        return [];
+    }
+    else{
+        return JSON.parse(basket)
+    }
+}
+function addBasket(product){
+    let basket = getBasket();
+    let foundProductId = basket.find(p => p.id == product.id);
+    let foundProductColor = basket.find(q => q.color == colors.value);
+    if(foundProductId == undefined){
+            product.quantity = parseInt(quantity.value);
+            basket.push(product)
+    }
+    else{
+        if(foundProductColor == undefined){
+            product.quantity = parseInt(quantity.value);
+            basket.push(product)
+        }
+        else{
+            let newQuantity = parseInt(foundProductColor.quantity) + parseInt(quantity.value)
+            foundProductColor.quantity = newQuantity
+        }
+    }
+    saveBasket(basket)
+}
+// ==========================================================================================================================
 const itemImgSelector = document.querySelector(".item__img")
+const titleSelector = document.querySelector("title")
 const idUrl = window.location.search.slice(4)
 const url = `http://localhost:3000/api/products/${idUrl}`
+// ==========================================================================================================================
 fetch(url)
 .then(response => response.json())
 .then(data => {
     itemImgSelector.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`
-    title.innerHTML = `${data.name}`
+    titleSelector.innerHTML = `${data.name}`
     price.innerHTML = `${data.price}`
     description.innerHTML = `${data.description}`
     document.title.textContent = `${data.name}`
     for(var i = 0; i<data.colors.length; i++){
         colors.innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`
-    }
-    console.log(typeof colors.value);
+    } 
     addToCart.addEventListener("click", () => {
-        if((colors.value === '')&&(quantity.value <= quantity.min || quantity.value > quantity.max)){
-            alert("veuillez renseigner une couleur et une quantitée")
+
+        if((colors.value === '')&&(quantity.value <= parseInt(quantity.min) || quantity.value > parseInt(quantity.max))){
+            alert("veuillez renseigner une couleur et une quantitée entre 1 et 100")
         }
         else{
             if(colors.value === ''){
                 alert("veuillez renseigner une couleur")
             }
-            else if(quantity.value > quantity.max || quantity.value <= quantity.min ){
+            else if(quantity.value > parseInt(quantity.max) || quantity.value <= parseInt(quantity.min) ){
                 alert("veuillez renseigner une quantitée entre 1 et 100")
             }
             else{
-                alert("Article(s) ajouté(s) au panier")
+                const order = 
+                {
+                    id : idUrl,
+                    "quantity" : quantity.value,
+                    "color" : colors.value
+                } 
+                addBasket(order)
+                alert("article(s) ajouté(s) au panier")
             }
         }
+
     })   
 })
